6d34961ce91e0e99e5277f726261f36d989432a6
Naoki Otani <e1710@s.akashi.ac.jp>
2018-06-28 20:40:22 +0900

    オセロの四近傍を裏返す処理を実装、汚い

--- src/naoppy/othello/OthelloField.java
+++ src/naoppy/othello/OthelloField.java
@@ -7,8 +7,8 @@ public class OthelloField {
     private boolean nextIsWhite = true;
 
     public OthelloField() {
-        for(int y = 0; y < 8; y++) {
-            for(int x = 0; x < 8; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 fieldMap[y][x] = Koma.NONE;
             }
         }
@@ -19,8 +19,8 @@ public class OthelloField {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(int y = 0; y < 8; y++) {
-            for(int x = 0; x < 8; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 switch (fieldMap[y][x]) {
                     case NONE:
                         sb.append(' ');
@@ -32,17 +32,64 @@ public class OthelloField {
                         sb.append('o');
                 }
             }
-            if(y!=7) sb.append('\n');
+            if (y != 7) sb.append('\n');
         }
 
         return sb.toString();
     }
 
+    static int[] lookingWayX = {1, 0, -1, 0};
+    static int[] lookingWayY = {0, 1, 0, -1};
+
     public boolean putNewKoma(int y, int x) {
-        return false;
+        boolean isUpdated = false;
+
+        //既にある場所に重ねて置けない
+        if (fieldMap[y][x] != Koma.NONE) return false;
+
+
+        for (int i = 0; i < 4; i++) {
+            int lookingY = y, lookingX = x;
+            lookingY += lookingWayY[i];
+            lookingX += lookingWayX[i];
+            if (fieldMap[lookingY][lookingX] == Koma.BLACK && nextIsWhite) {
+                while (fieldMap[lookingY][lookingX] == Koma.BLACK) {
+                    lookingY += lookingWayY[i];
+                    lookingX += lookingWayX[i];
+                }
+                if (fieldMap[lookingY][lookingX] == Koma.WHITE) {
+                    isUpdated |= true;
+                    while (!(lookingY == y && lookingX == x)) {
+                        fieldMap[lookingY][lookingX] = Koma.WHITE;
+                        lookingY -= lookingWayY[i];
+                        lookingX -= lookingWayX[i];
+                    }
+                    fieldMap[y][x] = Koma.WHITE;
+                }
+            } else if (fieldMap[lookingY][lookingX] == Koma.WHITE && !nextIsWhite) {
+                while (fieldMap[lookingY][lookingX] == Koma.WHITE) {
+                    lookingY += lookingWayY[i];
+                    lookingX += lookingWayX[i];
+                }
+                if (fieldMap[lookingY][lookingX] == Koma.BLACK) {
+                    isUpdated |= true;
+                    while (!(lookingY == y && lookingX == x)) {
+                        fieldMap[lookingY][lookingX] = Koma.BLACK;
+                        lookingY -= lookingWayY[i];
+                        lookingX -= lookingWayX[i];
+                    }
+                    fieldMap[y][x] = Koma.BLACK;
+                }
+            }
+        }
+        if (isUpdated) {
+            this.nextIsWhite = !this.nextIsWhite;
+        }
+
+        return isUpdated;
     }
 
-    public boolean getIsNextIsWhite() {
+    public boolean getNextIsWhite() {
         return this.nextIsWhite;
     }
 }
--- test/naoppy/othello/OthelloFieldTest.java
+++ test/naoppy/othello/OthelloFieldTest.java
@@ -1,5 +1,6 @@
 package naoppy.othello;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +24,27 @@ class OthelloFieldTest {
 
     @Test
     void nextIsWhiteTest() {
-        assertTrue(field.getIsNextIsWhite());
+        assertTrue(field.getNextIsWhite());
+    }
+
+    @Test
+    void putNewKomaCanPutUpperClose() {
+        assertTrue(field.putNewKoma(2,4), "縦に挟めるはず");
+    }
+
+    @Test
+    void putNewKomaUpdateUpperClose() {
+        field.putNewKoma(2,4);
+
+        String expectedString =
+                "        " +
+                        "\n        " +
+                        "\n    o   " +
+                        "\n   oo   " +
+                        "\n   xo   " +
+                        "\n        " +
+                        "\n        " +
+                        "\n        ";
+        assertEquals(expectedString, field.toString(), "縦に挟めなければいけない");
     }
 }
