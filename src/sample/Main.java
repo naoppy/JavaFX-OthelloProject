9c7e3fc2d0c20a443f7c98111236742482763336
Naoki Otani <e1710@s.akashi.ac.jp>
2018-06-30 21:53:59 +0900

    リソースを追加

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,31 +3,35 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import naoppy.othello.OthelloField;
+import naoppy.othello.OthelloPlayer;
 
 public class Main extends Application {
 
+    private OthelloField othelloField = new OthelloField();
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
         GridPane root = new GridPane();
-        root.setGridLinesVisible(true);
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
-                Group g = new Group();
-                g.getChildren().add(new Rectangle(50, 50, Color.WHITE));
-                root.add(g, y, x);
+                Image image = new Image(this.getClass().getResourceAsStream("../None.png"));
+                ImageView imageView = new ImageView(image);
+                root.add(new ImageView(image), y, x);
             }
         }
 
         primaryStage.setTitle("Naoppy Othello");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
-        System.err.println(root.isGridLinesVisible());
     }
 
 
